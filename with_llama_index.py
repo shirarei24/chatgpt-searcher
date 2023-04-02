971b00f80f9d369b1bdfa100c54bda15e05e5a0d
Reiki Shirasawa <periophthalmus24@gmail.com>
2023-04-02 17:56:36 +0900

    Add conversational agent

--- with_llama_index.py
+++ with_llama_index.py
@@ -1,33 +1,43 @@
-import argparse
-import os
-
 from dotenv import load_dotenv
+from langchain import OpenAI
+from langchain.agents import Tool, initialize_agent
+from langchain.agents.conversational import prompt
+from langchain.chains.conversation.memory import ConversationBufferMemory
 from llama_index import GPTSimpleVectorIndex, SimpleDirectoryReader
 
 load_dotenv(".env")
 
 
-def create_parser():
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--query",
-        "-q",
-        help="Querty string: e.g. '関門トンネルとはどことどこを結ぶトンネルですか？'",
-        required=True,
-    )
-    return parser
-
-
 def main():
-    parser = create_parser()
-    args = parser.parse_args()
     documents = SimpleDirectoryReader("data").load_data()
-    index = GPTSimpleVectorIndex.from_documents(documents)
+    index = GPTSimpleVectorIndex.from_documents(
+        documents,
+    )
 
-    index.save_to_disk("index.json")
-    index = GPTSimpleVectorIndex.load_from_disk("index.json")
+    tools = [
+        Tool(
+            name="Kanmon Tunnel",
+            func=lambda q: str(index.query(q)),
+            description="田中太郎の夕飯の献立を調べる際に利用することができます。",
+            return_direct=True,
+        ),
+    ]
 
-    print(index.query(args.query))
+    agent = initialize_agent(
+        tools=tools,
+        llm=OpenAI(temperature=0),
+        agent="conversational-react-description",
+        verbose=True,
+        memory=ConversationBufferMemory(memory_key="chat_history"),
+        agent_kwargs=dict(suffix="Answer should be in Japanese.¥n" + prompt.SUFFIX),
+    )
+
+    print(agent.agent.llm_chain.prompt.template)
+    while True:
+        user_input = input("input:")
+        if user_input == "exit":
+            break
+        print(agent.run(user_input))
 
 
 if __name__ == "__main__":
